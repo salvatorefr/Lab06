473f7d3b35ef4613917dc40fe05b1c34078804ad
tattommasi <47092404+tattommasi@users.noreply.github.com>
2019-05-12 12:38:04 +0200

    soluzione

--- src/it/polito/tdp/meteo/db/MeteoDAO.java
+++ src/it/polito/tdp/meteo/db/MeteoDAO.java
@@ -4,13 +4,85 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
+import it.polito.tdp.meteo.bean.Citta;
 import it.polito.tdp.meteo.bean.Rilevamento;
 
 public class MeteoDAO {
 
+	/**
+	 * Elenco di tutte le città presenti nel database.
+	 * @return
+	 */
+	public List<Citta> getAllCitta() {
+		String sql = "SELECT DISTINCT localita FROM situazione ORDER BY localita";
+
+		List<Citta> result = new ArrayList<>();
+
+		try {
+			Connection conn = DBConnect.getInstance().getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+
+			while (res.next()) {
+				result.add(new Citta(res.getString("localita")));
+			}
+
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	/**
+	 * Dato un mese ed una città, estrare dal DB l'umidità media relativa a tale mese e tale città.
+	 * <p>Tutti i calcoli sono delegati al database.
+	 * 
+	 * @param mese
+	 * @param citta
+	 * @return
+	 */
+	public Double getUmiditaMedia(Month mese, Citta citta) {
+
+		String sql = "SELECT AVG(Umidita) AS U FROM situazione " + 
+				"WHERE localita=? " + 
+				"AND MONTH(data)=? ";
+
+		try {
+			Connection conn = DBConnect.getInstance().getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+
+			st.setString(1, citta.getNome());
+			st.setInt(2, mese.getValue());
+
+			ResultSet res = st.executeQuery();
+
+			res.next(); // posiziona sulla prima (ed unica) riga
+
+			Double u = res.getDouble("U");
+
+			conn.close();
+			
+			return u;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	/**
+	 * Carica dal database l'elenco completo di tutti i rilevamenti
+	 * @return
+	 */
 	public List<Rilevamento> getAllRilevamenti() {
 
 		final String sql = "SELECT Localita, Data, Umidita FROM situazione ORDER BY data ASC";
@@ -39,14 +111,42 @@ public class MeteoDAO {
 		}
 	}
 
-	public List<Rilevamento> getAllRilevamentiLocalitaMese(int mese, String localita) {
+	/**
+	 * Carica i rilevamenti di una determinata città in un determinato mese.
+	 * 
+	 * @param mese
+	 * @param localita
+	 * @return
+	 */
+	public List<Rilevamento> getRilevamentiLocalitaMese(Month mese, Citta localita) {
+		
+		final String sql = "SELECT Localita, Data, Umidita FROM situazione WHERE MONTH(Data)=? AND Localita=? ORDER BY data ASC";
+		
+		List<Rilevamento> rilevamenti = new ArrayList<Rilevamento>();
 
-		return null;
-	}
+		try {
+			Connection conn = DBConnect.getInstance().getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			
+			st.setInt(1, mese.getValue());
+			st.setString(2, localita.getNome());
 
-	public Double getAvgRilevamentiLocalitaMese(int mese, String localita) {
+			ResultSet rs = st.executeQuery();
 
-		return 0.0;
+			while (rs.next()) {
+
+				Rilevamento r = new Rilevamento(rs.getString("Localita"), rs.getDate("Data"), rs.getInt("Umidita"));
+				rilevamenti.add(r);
+			}
+
+			conn.close();
+			return rilevamenti;
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
 	}
 
 }
