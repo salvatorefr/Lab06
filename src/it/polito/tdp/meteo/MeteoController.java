3c7b63763b158e18d0d0a9ab1d93558c9f58331b
tattommasi <47092404+tattommasi@users.noreply.github.com>
2019-05-12 12:33:40 +0200

    soluzione

--- src/it/polito/tdp/meteo/MeteoController.java
+++ src/it/polito/tdp/meteo/MeteoController.java
@@ -1,15 +1,23 @@
 package it.polito.tdp.meteo;
 
 import java.net.URL;
+import java.time.Month;
+import java.time.format.TextStyle;
+import java.util.List;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
+import it.polito.tdp.meteo.bean.Citta;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
+import javafx.util.StringConverter;
 
 public class MeteoController {
+	
+	private Model model ;
 
 	@FXML
 	private ResourceBundle resources;
@@ -18,7 +26,7 @@ public class MeteoController {
 	private URL location;
 
 	@FXML
-	private ChoiceBox<?> boxMese;
+	private ChoiceBox<Month> boxMese;
 
 	@FXML
 	private Button btnCalcola;
@@ -31,12 +39,29 @@ public class MeteoController {
 
 	@FXML
 	void doCalcolaSequenza(ActionEvent event) {
+		
+		Month m = boxMese.getValue() ;
+		if(m!=null) {
+			List<Citta> best = model.calcolaSequenza(m) ;
+			txtResult.appendText("Sequenza ottima per il mese "+m.toString()+"\n");
+			txtResult.appendText(best+"\n");
+		}
 
 	}
 
 	@FXML
 	void doCalcolaUmidita(ActionEvent event) {
 
+		Month m = boxMese.getValue() ;
+		if(m!=null) {
+			txtResult.appendText(String.format("Dati del mese %s\n", m.toString()));
+
+			for(Citta c: model.getLeCitta()) {
+				Double u = model.getUmiditaMedia(m, c) ;
+				txtResult.appendText(String.format("Città %s: umidità %f\n", c.getNome(), u));
+			}
+		}
+		
 	}
 
 	@FXML
@@ -45,6 +70,29 @@ public class MeteoController {
 		assert btnCalcola != null : "fx:id=\"btnCalcola\" was not injected: check your FXML file 'Meteo.fxml'.";
 		assert btnUmidita != null : "fx:id=\"btnUmidita\" was not injected: check your FXML file 'Meteo.fxml'.";
 		assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Meteo.fxml'.";
+		
+		// popola la boxMese con i 12 mesi dell'anno
+		for(int mese = 1; mese <= 12 ; mese ++)
+			boxMese.getItems().add(Month.of(mese)) ;
+		
+		// il setConverter serve a definire un metodo alternativo al toString nativo di <Month> per definire
+		// l'etichetta del bottone. In questo caso lo covertiamo utilizzando la lingua italiana.
+		// La ChoiceBox utilizzerà quindi il toString di questo StringConverter anziché quello di default.
+		boxMese.setConverter(new StringConverter<Month>() {
+			@Override
+			public String toString(Month m) {
+				return m.getDisplayName(TextStyle.FULL, Locale.ITALIAN) ;
+			}
+			
+			@Override
+			public Month fromString(String string) {
+				return null;
+			}
+		});
+	}
+	
+	public void setModel(Model m) {
+		this.model = m ;
 	}
 
 }
